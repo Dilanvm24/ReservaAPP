1d7cbfe5632f9156a0b57c224e1dc419ae66a180
alexis <romero.carlos6357@gmail.com>
2025-06-03 09:49:50 -0500

    se crean variables

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,45 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {System.out.println("Reservas APP");
+    public static void main(String[] args {
+             System.out.println("Reservas APP");
         System.out.println("==============\n");
-        }
+
+        //Entradas del algoritmo
+        Scanner entradaTeclado=new Scanner(System.in);
+        LocalDate fechaReserva;
+        LocalDate fechaMantenimiento;
+        String nombreUsuario;
+        String idUsuario;
+        String correoUsuario;
+        String tipoUsuario;
+        String numeroApartamento;
+        String tipoApartamentos;
+        String contraseñaUsuario;
+        String telefonoUsuario;
+
+        String horarioEspacio;
+        String nombreEspacio;
+        String reglamentoEspacio;
+        String utenasiliosEspacio;
+
+        String codigoReserva;
+        String horarioReserva;
+
+
+        byte numeroBloque;
+        byte cantidadAsistentes;
+        byte edadUsuario;
+        int valorReserva;
+
+        boolean esAdministrador=false;
+        boolean confirmacionReserva=false;
+        boolean estaOcupado=false;
+        boolean estaEnMantenimiento=false;
+
+    }
+
 }
